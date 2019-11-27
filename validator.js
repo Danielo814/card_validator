e78aa6eda326051db302dd5f1ee0423fbb779984
Danielo814 <ojedadaniel92@gmail.com>
2019-11-26 21:56:08 -0800

    add function for discover

--- validator.js
+++ validator.js
@@ -2,13 +2,14 @@
 
 function cardTypeCheck() {
   var num = document.getElementById("cardNum").value;
-  console.log(num.length);
-  if (num.length !== 16 && num.length !== 15) {
-    console.log("not a valid card number");
-  } else if (num.length == 15) {
+  if (num.length == 15 && num[0] == "3") {
     validateAmex(num);
   } else if ((num.length == 16 || num.length == 13) && num[0] == "4") {
     validateVisa(num);
+  } else if ((num.slice(0, 4) == "6011" || num[0] == "5") && num.length == 15) {
+    validateDiscover(num);
+  } else {
+    console.log("Not a valid card number");
   }
 }
 
@@ -16,8 +17,12 @@ function validateVisa(cardNum) {
   console.log("Type: Visa");
 }
 
+function validateDiscover(cardNum) {
+  console.log("Type: Discover");
+}
+
 function validateAmex(cardNum) {
-  if ((cardNum[0] !== "3" && cardNum[1] !== "4") || cardNum[1] !== "7") {
+  if (cardNum[0] !== "3" || (cardNum[1] !== "4" && cardNum[1] !== "7")) {
     console.log("not a valid card number");
   } else {
     console.log("Type: American Express");
